b47902e6b035d99b02a5f08bb8c82748e06e242f
TataRin <124790207+Tatarin61866@users.noreply.github.com>
2023-12-02 10:28:59 +0000

    Add files via upload

---
+++ lab08_2/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, flash, redirect, url_for, abort
+from datetime import datetime
+
+app = Flask('lab08')
+app.config.from_object('config')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        if request.form.get('password') == 'password':
+            return redirect(url_for('index'))
+        else:
+            return "Wrong password", 401
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/page1', methods=['GET', 'POST'])
+def page2():
+    return render_template('page3.html')
+
+@app.route('/page4', methods=['GET', 'POST'])
+def page5():
+    return render_template('page6.html')
+
+@app.route('/go_to_index')
+def redir():
+    p = request.args.get('pass')
+    if p != '123':
+        return abort(403)
+    return redirect(url_for('index'))
+
+@app.route('/send_msg', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        m = request.form.get('text')
+        flash(m)
+    return render_template('form.html')
+
+#app.run(host="0.0.0.0", port=5000, debug='true')
